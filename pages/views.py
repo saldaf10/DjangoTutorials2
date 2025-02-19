b97347fe7c9dcc694214e63b2d8689ca5019bd95
Pedro Saldarriaga <pedrosaldarriaga@MacBook-Air-de-Pedro.local>
2025-02-18 20:08:08 -0500

    Segundo tutorial completado

--- pages/views.py
+++ pages/views.py
@@ -1,13 +1,16 @@
-from django.shortcuts import render, redirect # here by default
-from django.views.generic import TemplateView
+from django.shortcuts import render, redirect, get_object_or_404 # here by default
+from django.views.generic import TemplateView, ListView
 from django.views import View
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django import forms
 
-class ProductForm(forms.Form):
-    name = forms.CharField(required=True)
-    price = forms.FloatField(required=True, min_value=0.01)
+from .models import Product
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ["name", "price"]
 
 class ProductCreateView(View):
     template_name = 'products/create.html'
@@ -22,6 +25,7 @@ class ProductCreateView(View):
         form = ProductForm(request.POST)
         if form.is_valid():
         # Aquí podrías guardar el producto si estuvieras usando un modelo
+            form.save()
             return redirect(reverse('index'))  # Redirige a la lista de productos
         else:
             viewData = {}
@@ -29,13 +33,6 @@ class ProductCreateView(View):
             viewData["form"] = form
             return render(request, self.template_name, viewData)
 
-class Product:
-    products = [
-        {"id":"1", "name":"TV", "description":"Best TV", "price":"1000"},
-        {"id":"2", "name":"iPhone", "description":"Best iPhone", "price":"1000"},
-        {"id":"3", "name":"Chromecast", "description":"Best Chromecast", "price":"1000"},
-        {"id":"4", "name":"Glasses", "description":"Best Glasses", "price":"1000"}
-]
     
 class ProductIndexView(View):
     template_name = 'products/index.html'
@@ -44,26 +41,40 @@ class ProductIndexView(View):
         viewData = {}
         viewData["title"] = "Products - Online Store"
         viewData["subtitle"] = "List of products"
-        viewData["products"] = Product.products
+        viewData["products"] = Product.objects.all()
         return render(request, self.template_name, viewData)
     
 
 class ProductShowView(View):
     template_name = 'products/show.html'
-    
-    def get(self, request, id):
-        viewData = {}
-        # Verificar si el id es válido
-        if int(id) < 1 or int(id) > len(Product.products):
-            # Redireccionar a la página de inicio si el id no es válido
-            return HttpResponseRedirect(reverse('home'))
 
-        product = Product.products[int(id)-1]
-        viewData["title"] = product["name"] + " - Online Store"
-        viewData["subtitle"] = product["name"] + " - Product information"
+    def get(self, request, id):
+    # Check if product id is valid
+        try:
+            product_id = int(id)
+            if product_id < 1:
+                raise ValueError("Product id must be 1 or greater")
+            product = get_object_or_404(Product, pk=product_id)
+        except (ValueError, IndexError):
+        # If the product id is not valid, redirect to the home page
+            return HttpResponseRedirect(reverse('home'))
+        viewData = {}
+        product = get_object_or_404(Product, pk=product_id)
+        viewData["title"] = product.name + " - Online Store"
+        viewData["subtitle"] = product.name + " - Product information"
         viewData["product"] = product
-        viewData["price"] = float(product["price"])
         return render(request, self.template_name, viewData)
+    
+class ProductListView(ListView):
+    model = Product
+    template_name = 'product_list.html'
+    context_object_name = 'products' # This will allow you to loop through 'products' in your template
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Products - Online Store'
+        context['subtitle'] = 'List of products'
+        return context
 
 class HomePageView(TemplateView):
     template_name = 'pages/home.html'
